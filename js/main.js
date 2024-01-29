3ed902cbfef229d92d09fda7e47f571b1e8adaa1
watermelonhero <watermelonhero@duck.com>
2024-01-28 21:49:46 -0800

    Container margin, content padding, added more clue1 clues

--- js/main.js
+++ js/main.js
@@ -40,11 +40,13 @@ const getRandomCiv = () => {
 
     const { clue1, clue2, clue3, clue4, clue5 } = clues[0];
 
-    // Randomly picks 3 random clue1 clues and storee the first clue to display
+    // Randomly picks 3 random clue1 clues and stores the first clue to display
     const shuffledClue1 = [...new Set(clue1.sort(() => Math.random() - 0.3))];
     clueDisplays[0].textContent = shuffledClue1[0];
     clueDisplays[1].textContent = shuffledClue1[1];
     clueDisplays[2].textContent = shuffledClue1[2];
+    clueDisplays[3].textContent = shuffledClue1[3];
+    clueDisplays[4].textContent = shuffledClue1[4];
 
     sharedData.clue2 = clue2;
     sharedData.clue3 = clue3;
@@ -60,8 +62,8 @@ const revealNextClue = () => {
     switch (currentClueIndex) {
         case 3: // Reveal second clue from clue2
             const shuffledClue2 = [...new Set(sharedData.clue2.sort(() => Math.random() - 0.5))];
-            clueDisplays[3].textContent = shuffledClue2[0];
-            clueDisplays[4].textContent = shuffledClue2[1];
+            clueDisplays[5].textContent = shuffledClue2[0];
+            clueDisplays[6].textContent = shuffledClue2[1];
 
             // Reveal the clue by setting its opacity to 1
             clueElements[1].style.opacity = 1;
@@ -69,8 +71,8 @@ const revealNextClue = () => {
 
         case 4: // Reveal next clue from clue3
             const { civbonus, civuniquetech } = sharedData.clue3[0];
-            clueDisplays[5].textContent = civbonus[Math.floor(Math.random() * civbonus.length)];
-            clueDisplays[6].textContent = civuniquetech[Math.floor(Math.random() * civuniquetech.length)];
+            clueDisplays[7].textContent = civbonus[Math.floor(Math.random() * civbonus.length)];
+            clueDisplays[8].textContent = civuniquetech[Math.floor(Math.random() * civuniquetech.length)];
 
             // Reveal the clue by setting its opacity to 1
             clueElements[2].style.opacity = 1;
@@ -79,14 +81,14 @@ const revealNextClue = () => {
 
         case 5: // Reveal clue4
             // Clue4 is only 1 clue
-            clueDisplays[7].textContent = sharedData.clue4[0];
+            clueDisplays[9].textContent = sharedData.clue4[0];
 
             // Reveal the clue by setting its opacity to 1
             clueElements[3].style.opacity = 1;
             break;
 
         case 6: // Reveal clue5
-            clueDisplays[8].textContent = sharedData.clue5.length === 1 ? sharedData.clue5[0] : sharedData.clue5[Math.floor(Math.random() * sharedData.clue5.length)];
+            clueDisplays[10].textContent = sharedData.clue5.length === 1 ? sharedData.clue5[0] : sharedData.clue5[Math.floor(Math.random() * sharedData.clue5.length)];
 
             // Reveal the clue by setting its opacity to 1
             clueElements[4].style.opacity = 1;
